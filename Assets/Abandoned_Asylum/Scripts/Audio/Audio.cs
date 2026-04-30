c481ca819a7d15e1b6f839c8b74969813bcc5446
ElAlbany <zajdhany112@gmail.com>
2026-04-30 19:02:06 +0300

    sounds finished ElAlbany

--- Assets/Abandoned_Asylum/Scripts/Audio/Audio.cs
+++ Assets/Abandoned_Asylum/Scripts/Audio/Audio.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+public class Audio : MonoBehaviour
 {
-    public static AudioManager Instance;
+    public static Audio Instance;
 
     [Header("Audio Sources")]
     public AudioSource starterSource;
